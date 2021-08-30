f76bdd6f3e0ca56769c3f2cf1e207032a9e68d5e
Admin <nematovbahrom949>
2021-08-30 20:43:51 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mydemo4/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        HomePage.id : (context) => HomePage()
+      },
+    );
+  }
+}
+
